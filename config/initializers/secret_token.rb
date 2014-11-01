39c361a4e51a059eb4ec42f72c80ae124e2dbffd
Jonathan Wexler <Jwexman@gmail.com>
2014-11-01 16:02:57 -0400

    first commit clone of snappyquiz with working transition buttons and phone mode

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snappyquiz2::Application.config.secret_key_base = 'd565fc8b10b04412f49268c938f5f689ab56068beb3925e66440f48ea640416c7608e6019932c15c6047d43147fbfafd764f314e60a995a62552d166361ae683'
